59aea7d03667862fc4a39cae3bde7a44636c0b87
Pavel Serebrennikov <Serebrennikov.p.d@gmail.com>
2023-06-04 20:21:21 +0300

    WIP

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import BG from '../images/Zombie-Cats_BG.png'
+import Cat1 from "../images/Zombie-cat-1.png"
+import "../CSS/Game.css"
 
 const Game = () => {
 
   return (
-    <div className="Game">
+    <div className="game-wrapper">
+      <div className="header-line"></div>
+      <div className="Game" style={{
+        backgroundImage: `url(${BG})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: '100%',
+        height: 'calc(100vw / (16 / 9))',
+        overflow: 'hidden'
+      }}>
+
+        <img className="cat1 line3" src={Cat1} />
+        <img className="cat1 line5" src={Cat1} />
+      </div>
+      <div className="footer-line"></div>
     </div>
   );
 };
