5963dd24252e8cceb77c893f5d782c8177dfc33f
shlomozion <jordan23zion@gmail.com>
2023-06-04 22:01:08 +0300

    added font and backround

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,17 +1,32 @@
 import React from "react";
 import { Kbd } from "@chakra-ui/layout";
 import { useNavigate } from "react-router-dom";
+import darkforest from "../images/darkforest.gif";
+import "../CSS/CustomFont.css";
 
 const HomePage = () => {
+  console.log(darkforest);
   const navigate = useNavigate();
   return (
-    <div className="home-page">
+    <div
+      className="home-page"
+      style={{
+        backgroundImage: `url(${darkforest})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
       <div className="logo-banner">
         <img src="" alt="game-logo" />
       </div>
       <div className="game-instructions">
         <div className="rules-of-game">
-          <div className="story">Story:</div>
+          <div
+            className="story"
+            style={{ fontFamily: "CustomFont", fontSize: "2rem" }}
+          >
+            Story:
+          </div>
           <div>
             In Pleasantville, a once peaceful town, a catastrophic experiment at
             a local laboratory has unleashed a virus that turned cats into
@@ -28,7 +43,7 @@ const HomePage = () => {
             <b>+</b>
             <Kbd>{`Left arrow -->`}</Kbd>
           </div>
-          <div>
+          <div className="instructions">
             To send healing heart use the <Kbd className="kbd">{`space`}</Kbd>
           </div>
         </div>
