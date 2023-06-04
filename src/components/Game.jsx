0a490fdaab4df08a4ba72876a82ceff5668ef57a
Pavel Serebrennikov <Serebrennikov.p.d@gmail.com>
2023-06-04 21:59:11 +0300

    all cats

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -6,20 +6,18 @@ import "../CSS/Game.css"
 const Game = () => {
 
   return (
-    <div className="game-wrapper">
-      <div className="header-line"></div>
-      <div className="Game" style={{
-        backgroundImage: `url(${BG})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '100%',
-        height: 'calc(100vw / (16 / 9))',
-        overflow: 'hidden'
-      }}>
-
-        <img className="cat1 line3" src={Cat1} />
-        <img className="cat1 line5" src={Cat1} />
-      </div>
-      <div className="footer-line"></div>
+    <div className="Game" style={{
+      backgroundImage: `url(${BG})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: '100%',
+      height: 'calc(100vw / (16 / 9))',
+      overflow: 'hidden'
+    }}>
+      <img className="cat1 line1" src={Cat1} />
+      <img className="cat1 line2" src={Cat1} />
+      <img className="cat1 line3" src={Cat1} />
+      <img className="cat1 line4" src={Cat1} />
+      <img className="cat1 line5" src={Cat1} />
     </div>
   );
 };
