230455be8e7e7dae372a0f0655b4d73e29b4a295
BumBox <xbumbox@mail.ru>
2023-06-05 16:56:34 +0300

    Added fetch functionality

--- src/components/ScoreboardPage.jsx
+++ src/components/ScoreboardPage.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "../CSS/ScoreBoard.css";
-
-
-
-
+import axios from "axios";
 
 const ScoreboardPage = () => {
-  return (
-    <div>
-        <div className='backgroundSquare'></div>
-    </div>
-  )
-}
+  const [scoresList, setScoresList] = useState([]);
 
-export default ScoreboardPage
+  const getScore = async () => {
+    try {
+      const res = await axios.get("http://localhost:8080/scoreboard");
+      setScoresList(res.data);
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getScore();
+  }, []);
+
+  return (
+    <div className="backgroundSquare">
+      <button onClick={getScore}> Get score</button>
+      <div className="ScoreboardTabble">
+        {scoresList.map((user) => {
+          console.log("userAdded");
+          <div className="ScoreboardTabbleUser">
+            {user.Nickname} {user.Scores}
+          </div>;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default ScoreboardPage;
