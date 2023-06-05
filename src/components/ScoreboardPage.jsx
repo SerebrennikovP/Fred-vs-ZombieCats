1fe10b8995e372a15a332779da265d9c99bcc3e9
BumBox <xbumbox@mail.ru>
2023-06-05 16:25:10 +0300

    ScoreBoard creation

---
+++ src/components/ScoreboardPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../CSS/ScoreBoard.css";
+
+
+
+
+
+const ScoreboardPage = () => {
+  return (
+    <div>
+        <div className='backgroundSquare'></div>
+    </div>
+  )
+}
+
+export default ScoreboardPage
